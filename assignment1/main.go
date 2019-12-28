0b1d3b30aecb600617b6252af8825d01fd695127
Vicky Singh <vickysingh@vickys-mac.local>
2019-12-29 05:04:47 +0530

    Fixed check issue

--- assignment1/main.go
+++ assignment1/main.go
@@ -16,19 +16,19 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 func isPrime(origNum int) bool {
 	num := math.Abs(float64(origNum))
 	result := true
-	if num < 2 || num == 4{
+	if num < 2 || num == 4 {
 		result = false
-	}else if num > 4{
+	} else if num > 4 {
 		limit := int(math.Sqrt(float64(num)))
 		// fmt.Println("limit", limit)
 		for i := 2; i <= limit; i++ {
-			if int(num) % i == 0 {
+			if int(num)%i == 0 {
 				result = false
-				break;
+				break
 			}
 		}
 	}
-	
+
 	fmt.Printf("isPrime check for %v returned %v\n", origNum, result)
 	return result
 
@@ -42,22 +42,22 @@ func isTwoSidedPrime(origNum int) bool {
 		return result
 	}
 
-	num1 := num / 10
-	check1 := isPrime(num1)
-	if !check1 {
-		result = false;
-		return result;		
+	for num1 := num / 10; num1 > 0; num1 = num1 / 10 {
+		check1 := isPrime(num1)
+		if !check1 {
+			result = false
+			return result
+		}
 	}
 
-	numdigits := int(math.Log10(float64(num))) + 1
-	// fmt.Println("Numdigits: ", numdigits)
-	num2 := num % int(math.Pow10(numdigits - 1))
-	check2 := isPrime(num2)
-	// fmt.Println("check2: ", check2)
-	if !check2 {
-		result = false;
+	for num2 := num % int(math.Pow10(int(math.Log10(float64(num))))); num2 > 0; num2 = num2 % int(math.Pow10(int(math.Log10(float64(num2))))) {
+		check2 := isPrime(num2)
+		if !check2 {
+			result = false
+		}
 	}
-	return result;
+
+	return result
 }
 
 func isTwoSidedPrimeHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,7 +67,7 @@ func isTwoSidedPrimeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Failed to convert string %s to integer", strNum)
 	}
 	result := isTwoSidedPrime(num)
-	fmt.Fprintf(w, strconv.FormatBool(result)) 	
+	fmt.Fprintf(w, strconv.FormatBool(result))
 }
 
 func main() {
